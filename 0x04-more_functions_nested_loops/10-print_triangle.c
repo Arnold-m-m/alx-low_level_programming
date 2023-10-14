2007639371365a80271d8e5e0c74f1618265e371
Arnold-m-m <mwangiarnold0@gmail.com>
2023-10-13 17:04:14 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
  * print_triangle - entry point
- * owned by Arnold
+ * owned by Arnold mwangi
  * Description: Prints diagonals
  *@size: size of the triangle
  * Return: void
@@ -15,4 +15,20 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
